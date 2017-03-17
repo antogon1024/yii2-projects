b057689307ae673bdc95a3493449bade387b5f52
antogon1024 <volck.sergej2012@yandex.ru>
2017-03-17 10:52:13 +0600

    Добавил модуль - mycms

--- config/web.php
+++ config/web.php
@@ -5,7 +5,14 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    
+	'modules' => [
+		'mycms' => [
+            'class' => 'app\modules\mycms\module',
+        ],
+    ],
+	
+	'bootstrap' => ['log'],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -43,6 +50,8 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+				//'<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+				
             ],
         ],
         */
---
+++ modules/mycms/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\mycms;
+
+/**
+ * mycms module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\mycms\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
